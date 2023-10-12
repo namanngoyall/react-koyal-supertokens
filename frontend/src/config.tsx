3c82faa883c89a3430430528b1dd20d655daaecc
Naman Goyal <namangoyal7682@gmail.com>
2023-10-12 10:33:37 +0530

    Skeleton code

---
+++ frontend/src/config.tsx
@@ -0,0 +1,48 @@
+import ThirdPartyEmailPassword, {
+    Google,
+    Github,
+    Apple,
+    Twitter,
+} from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import { ThirdPartyEmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/thirdpartyemailpassword/prebuiltui";
+import Session from "supertokens-auth-react/recipe/session";
+
+export function getApiDomain() {
+    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+export function getWebsiteDomain() {
+    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    return websiteUrl;
+}
+
+export const SuperTokensConfig = {
+    appInfo: {
+        appName: "React App by Koyal Tech LLP",
+        apiDomain: getApiDomain(),
+        websiteDomain: getWebsiteDomain(),
+    },
+    // recipeList contains all the modules that you want to
+    // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+    recipeList: [
+        ThirdPartyEmailPassword.init({
+            signInAndUpFeature: {
+                providers: [Github.init(), Google.init(), Apple.init(), Twitter.init()],
+            },
+        }),
+        Session.init(),
+    ],
+};
+
+export const recipeDetails = {
+    docsLink: "https://supertokens.com/docs/thirdpartyemailpassword/introduction",
+};
+
+export const PreBuiltUIList = [ThirdPartyEmailPasswordPreBuiltUI];
+
+export const ComponentWrapper = (props: { children: JSX.Element }): JSX.Element => {
+    return props.children;
+};
